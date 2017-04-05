87771870cc8c89f6e8d94a1bb0f3233f54f5681f
casLu <lucas_trabach@hotmail.com>
2017-04-05 20:29:57 -0300

    Atualizado

--- Model/Model/Produto.cs
+++ Model/Model/Produto.cs
@@ -8,5 +8,9 @@ namespace Model
 {
     class Produto
     {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public int Quantidade { get; set; }
     }
 }
