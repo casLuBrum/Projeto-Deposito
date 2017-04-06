11ae9b0de7455670b789a70b5b2d375c67d09469
nathantamagno <nathantamagno@gmail.com>
2017-04-05 21:43:34 -0300

    Criando classe cliente

---
+++ Model/Model/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
