9f238fe28e1708aa7cb2ec5ff14c52806555b88b
nathantamagno <nathantamagno@gmail.com>
2017-04-06 11:23:46 -0300

    Classe ClienteController feita ( sem erros ;P )

--- Model/Controller/ClienteController.cs
+++ Model/Controller/ClienteController.cs
@@ -3,12 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace Controller
 {
-    class ClienteController
+    public class ClienteController
     {
+        private static List<Cliente> listaCliente = new List<Cliente>();
+        public void Adicionar(string Nome, string CPF)
+        {
+            Cliente cli = new Cliente();
+            cli.ClienteID = listaCliente.Count + 1;
+            cli.Nome = Nome;
+            cli.CPF = CPF;
 
+            listaCliente.Add(cli);
+            
+        }
 
+        private Cliente BuscarPorID(int ID)
+        {
+            foreach ( Cliente cli in listaCliente)
+            {
+                if(cli.ClienteID == ID)
+                {
+                    return cli;
+                }
+            }
+            return null;
+        }
+        public Cliente Detalhes(int ID)
+        {
+            return BuscarPorID(ID);
+        }
+        public void Editar(int ID, string novoNome , string novoCPF)
+        {
+            Cliente C = BuscarPorID(ID);
+
+            if( C != null)
+            {
+                C.Nome = novoNome;
+                C.CPF = novoCPF;
+            }
+        }
+        public void Excluir(int ID)
+        {
+            foreach(Cliente cli in listaCliente)
+            {
+                if(cli.ClienteID == ID)
+                {
+                    listaCliente.Remove(cli);
+                }
+            }
+        }
+        public List<Cliente> Listar()
+        {
+            return listaCliente;
+        }
     }
 }
--- Model/Controller/ProdutoController.cs
+++ Model/Controller/ProdutoController.cs
@@ -8,6 +8,6 @@ namespace Controller
 {
     class ProdutoController
     {
-
+        
     }
 }
