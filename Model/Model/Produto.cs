cb1939e4935fd4153113d486e5ba7b206322191b
casLu <lucas_trabach@hotmail.com>
2017-04-12 22:06:21 -0300

    Criado String de conexão, anotações na classe Produto.
    Atualizando classe ProdutoController.

--- Model/Model/Produto.cs
+++ Model/Model/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace Model
 {
     public class Produto
     {
+        [Key]
         public int ProdutoID { get; set; }
+
+        [Required]
         public Cliente ClienteID { get; set; }
+
+        [Required, StringLength(30)]
         public string Nome { get; set; }
+
+        [Required, StringLength(10)]
         public double Peso { get; set; }
+
+        [Required, StringLength(10)]
         public int Quantidade { get; set; }
     }
 }
