34a28a5c0f2c2313eefcbbae0b859680a0b266f2
Lucas Brum <lucas_trabach@hotmail.com>
2017-04-18 19:07:31 -0300

    Atualizando Wpfs.

--- Model/Model/Cliente.cs
+++ Model/Model/Cliente.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Model
 {
     public class Cliente
     {
+        [Key]
         public int ClienteID { get; set; }
+
+        [Required, StringLength(30)]
         public string Nome { get; set; }
+        [Required, StringLength(30)]
         public string CPF { get; set; }
     }
 }
---
+++ Model/View/WpfCadastroCliente.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Model;
+using Controller;
+
+namespace View
+{
+    /// <summary>
+    /// Interação lógica para WpfCadastroCliente.xam
+    /// </summary>
+    public partial class WpfCadastroCliente : UserControl
+    {
+        public WpfCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salvar_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente c = new Cliente();
+
+            c.Nome = nome_txt.Text;
+            c.CPF = cpf_txt.Text;
+
+            ClienteController.Salvar(c);
+        }
+    }
+}
