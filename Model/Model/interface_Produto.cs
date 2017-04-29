5ef34d10feaff70ad3b124aeab2e6da7bee339ad
nathantamagno <nathantamagno@gmail.com>
2017-04-28 21:42:33 -0300

    Classe interface ( cliente e produto)

---
+++ Model/Model/interface_Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    interface interface_Produto
+    {
+
+        string Nome();
+        double Peso();
+    }
+}
